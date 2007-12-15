f72289dceef9e3378d4b5b511b25f9e19972d47b
spullara <spullara@a323b010-d439-0410-9a3b-5b3e18aa4ead>
2007-12-15 21:27:59 +0000

    convert to maven project

--- trunk/src/test/java/com/moonspider/dbmap/GenerateBeansTest.java
+++ trunk/src/test/java/com/moonspider/dbmap/GenerateBeansTest.java
@@ -44,7 +44,7 @@ public class GenerateBeansTest extends TestCase {
         p.put("password", "");
         Connection conn = driver.connect("jdbc:h2:mem:test_migration;DB_CLOSE_DELAY=-1", p);
         Migrate.sqlScriptMigrator(conn, "test-data/simplelock.sql");
-        Generator.main(new String[] { "test-src/generate.xml" });
+        Generator.main(new String[] { "target/test-classes/generate.xml" });
     }
 
     public void testGenerateGORM() throws Exception {
